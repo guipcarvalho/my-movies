3728b893d3dac05d3b15480dfb29176d808f53d4
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2022-01-29 17:01:45 -0300

    Add movies request

---
+++ src/api/MoviesService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = '53d763b5d2bc63b63e7ac4407a77d232';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios.get(withBaseUrl('movie/popular'));
+    }
+}
